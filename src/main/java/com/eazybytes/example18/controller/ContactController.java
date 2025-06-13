47434fb13b37868bbde303266bc8eb6e49349f53
himanshu-dhakate <dhakate.himanshu9@gmail.com>
2025-06-13 23:06:39 +0530

    Adding validation to send data in contact page

--- src/main/java/com/eazybytes/example18/controller/ContactController.java
+++ src/main/java/com/eazybytes/example18/controller/ContactController.java
@@ -2,17 +2,19 @@ package com.eazybytes.example18.controller;
 
 import com.eazybytes.example18.model.Contact;
 import com.eazybytes.example18.service.ContactService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.logging.Logger;
 
+@Slf4j
 @Controller
 public class ContactController {
     private ContactService contactService;
@@ -23,7 +25,8 @@ public class ContactController {
     }
 
     @RequestMapping("/contact")
-    public String displayContact() {
+    public String displayContact(Model model) {
+        model.addAttribute("contact", new Contact());
         return "contact.html";
     }
 
@@ -40,8 +43,12 @@ public class ContactController {
 //    }
 
     @PostMapping("/saveMsg")
-    public ModelAndView saveMessage(Contact contact) {
+    public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors) {
+        if (errors.hasErrors()) {
+            log.error("Contact form validation failed due to: " + errors.toString());
+            return "contact.html";
+        }
         contactService.saveMessage(contact);
-        return new ModelAndView("redirect:/contact");
+        return "redirect:/contact";
     }
 }
--- src/main/java/com/eazybytes/example18/model/Contact.java
+++ src/main/java/com/eazybytes/example18/model/Contact.java
@@ -1,12 +1,30 @@
 package com.eazybytes.example18.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Contact {
+    @NotBlank(message = "Name must not be blank")
+    @Size(min = 3, message = "Name must be atleast 3 characters long")
     private String name;
+
+    @NotBlank(message = "Mobile number must not be blank")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Invalid mobile number")
     private String mobileNum;
+
+    @NotBlank(message = "Email must not be blank")
+    @Email(message = "Invalid email address")
     private String email;
+
+    @NotBlank(message = "Subject cannot be blank")
+    @Size(min = 5, message = "Subject must be atleast 5 characters long")
     private String subject;
+
+    @NotBlank(message = "Message cannot be blank")
+    @Size(min = 10, message = "Message must be atleast 10 characters long")
     private String message;
 }
